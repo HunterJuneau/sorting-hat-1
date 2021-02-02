662d05d3291686a19c98de4b78cb78e167f232ba
HunterJuneau <huntlee17@gmail.com>
2021-02-01 18:44:06 -0600

    Created form and submit to students array.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const students = [];
+
 const houses = [
   {
     name: 'Gryffindor',
@@ -37,4 +39,49 @@ const houses = [
   },
 ];
 
-const students = [];
+const printToDom = (selector, text) => {
+  const selectedDiv = document.querySelector(selector);
+  selectedDiv.innerHTML = text;
+};
+
+const buildForm = () => {
+  let domString = '';
+
+  domString += `<form class="w-50 mx-auto my-3 p-3 border border-dark rounded" id="form">
+                  <div class="mb-3">
+                    <label for="student" class="form-label">Student Name</label>
+                    <input type="text" class="form-control" id="student" name="student">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Sort</button>
+                </form>`;
+
+  printToDom('#form', domString);
+};
+
+const getRandomHouse = () => {
+  return houses[Math.floor(Math.random() * 4)];
+};
+
+const sortStudent = (e) => {
+  e.preventDefault();
+
+  const studentName = document.querySelector('#student').value;
+
+  const student = {
+    name: studentName,
+    house: getRandomHouse(),
+  };
+
+  students.push(student);
+};
+
+const eventListeners = () => {
+  document.querySelector('#formButton').addEventListener('click', buildForm);
+  document.querySelector('#form').addEventListener('submit', sortStudent);
+};
+
+const init = () => {
+  eventListeners();
+};
+
+init();
